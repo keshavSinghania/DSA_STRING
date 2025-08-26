8d53e8774cbfc01a9b33f172aadbcc7ba3da9416
Keshav Singhania <worksinghania@gmail.com>
2025-08-26 17:35:31 +0530

    return true if given string s is anagram of string t

---
+++ src/ValidAnagram.java
@@ -0,0 +1,20 @@
+public class ValidAnagram {
+}
+//g
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         //sortting String s
+//         char[] arr1 = s.toCharArray();
+//         Arrays.sort(arr1);
+//         s= new String(arr1);
+
+//         //sortting String s
+//         char[] arr2 = t.toCharArray();
+//         Arrays.sort(arr2);
+//         t= new String(arr2);
+
+//         return (s.equals(t));
+//     }
+// }
