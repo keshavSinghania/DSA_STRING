ecba8bb667a10bfd2cf38f955657d9787379461c
Keshav Singhania <worksinghania@gmail.com>
2025-08-26 18:47:34 +0530

    maximum nesting depth of the parentheses

---
+++ src/MaxNestedParentheses.java
@@ -0,0 +1,20 @@
+public class MaxNestedParentheses {
+}
+
+//class Solution {
+//    public int maxDepth(String s) {
+//        int count =0;
+//        int maxCount = 0;
+//        for(int i=0; i<s.length(); i++){
+//            if(s.charAt(i)=='('){
+//                count++;
+//                if(count>maxCount){
+//                    maxCount = count;
+//                }
+//            }else if(s.charAt(i)==')'){
+//                count--;
+//            }
+//        }
+//        return maxCount;
+//    }
+//}
