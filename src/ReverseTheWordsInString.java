22913ee6552c088d765b8c564599de83244f4e45
Keshav Singhania <worksinghania@gmail.com>
2025-08-26 11:04:54 +0530

    Reverse the words in string

--- src/ReverseTheWordsInString.java
+++ src/ReverseTheWordsInString.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class ReverseTheWordsInString {
 }
+//https://leetcode.com/problems/reverse-words-in-a-string/
+//class Solution {
+//    public String reverseWords(String s) {
+//        //first trim string on basic of space and store as String array
+//        //trim == removing extra spaces from start and end
+//        //split == divide strig based on "\\s+" that is one or more extra spaces
+//        String [] strArr = s.trim().split(" +");
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        //will pick string from the back index of string array and append to sb
+//        for(int i= strArr.length-1; i>=0; i--){
+//            sb.append(strArr[i]);
+//
+//            //after every word , we must have add one space
+//            //NOTE : - donot add space after last word
+//            if(i>0){
+//                sb.append(" ");
+//            }
+//        }
+//        return sb.toString();
+//    }
+//}
